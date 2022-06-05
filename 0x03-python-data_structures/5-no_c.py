ed22ebe5895188e479086f1fb8d962ee556bf382
Yayeks <anhandle1@gmail.com>
2022-06-05 14:00:35 -0700

    Task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = [x for x in my_string if x != 'c' or x != 'C']
+    new_string = [x for x in my_string if x.lower() != 'c']
     return ("".join(new_string))
