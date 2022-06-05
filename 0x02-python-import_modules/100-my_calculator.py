92861ce613fb63157c330b33c1e19ea24030d56d
Yayeks <anhandle1@gmail.com>
2022-06-05 14:33:46 -0700

    Task6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    operator = {"+": add, "-": sub, "*": mul, "/": div}
+    
     from calculator_1 import add, sub, mul, div
     from sys import argv, exit
+    operator = {"+": add, "-": sub, "*": mul, "/": div}
 
     if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit 1
+        exit(1)
 
     if argv[2] not in list(operator.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit 1
+        exit(1)
 
     a = int(argv[1])
     b = int(argv[3])
