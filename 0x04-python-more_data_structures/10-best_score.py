b1c5562d4079e598fa47e45a85fd332e6d251150
Yayeks <anhandle1@gmail.com>
2022-06-08 13:28:38 -0700

    Task10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0::
+    if a_dictionary == None or len(a_dictionary) == 0::
         return None
     else:
         for key, value in a_dictionary.items():
