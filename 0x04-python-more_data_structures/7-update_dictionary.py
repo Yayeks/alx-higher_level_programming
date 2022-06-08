3373162821ecd7f438d9d5c5c8135a51c93a4a16
Yayeks <anhandle1@gmail.com>
2022-06-08 13:49:18 -0700

    Task7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,4 +2,8 @@
 def update_dictionary(a_dictionary, key, value):
     if type(key) == type('str'):
         a_dictionary[key] = value
+    for k in a_dictionary.keys():
+        if type(k) != type('str'):
+            return None
+        break
     return (a_dictionary)
