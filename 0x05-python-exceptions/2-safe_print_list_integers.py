7698128b1ec8f3947e1ceee9d13140b7535afcad
Yayeks <anhandle1@gmail.com>
2022-06-20 13:24:54 -0700

    Task2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             int_only += 1
         except (ValueError, TypeError):
             continue
-        print("")
-        return (int_only)
+    print("")
+    return (int_only)
