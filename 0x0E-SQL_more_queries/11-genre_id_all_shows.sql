1f9b992a19389d315d388efbff1b6f3ea55d8c01
Yayeks <anhandle1@gmail.com>
2022-08-11 12:49:47 -0700

    Tasks

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,4 @@
+-- lists all shows contained in the database hbtn_0d_tvshows
+SELECT t1.`title`, t2.`genre_id` FROM `tv_shows` AS t1 LEFT JOIN
+`tv_show_genres` AS t2 ON t1.`id` = t2.`show_id` ORDER BY t1.`title`,
+t2.`genre_id`;
---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,4 @@
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked
+SELECT t1.`title`, t2.`genre_id` FROM `tv_shows` AS t1 LEFT JOIN
+`tv_show_genres` AS t2 ON t1.`id` = t2.`show_id` WHERE t2.`genre_id` IS NULL
+ORDER BY t1.`title`, t2.`genre_id`;
