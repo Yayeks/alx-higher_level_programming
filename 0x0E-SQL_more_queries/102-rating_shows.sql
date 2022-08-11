bd85d9e080bb2dc2f7a83dc03e2f79abc7eb836b
Yayeks <anhandle1@gmail.com>
2022-08-11 15:07:32 -0700

    Task18

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,7 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating
-SELECT `title`, SUM(`rate`) AS `rating` FROM `tv_shows` AS t1
-INNER JOIN `tv_show_ratings` AS t2 ON t.`id` = t2.`show_id`
-GROUP BY `title` ORDER BY `rating` DESC;
+SELECT `title`, SUM(`rate`) AS `rating`
+FROM `tv_shows` AS t1
+INNER JOIN `tv_show_ratings` AS t2
+ON t1.`id` = t2.`show_id`
+GROUP BY `title`
+ORDER BY `rating` DESC;
